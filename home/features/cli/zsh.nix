974b3daac492810606f3c3d59d3ebcaa9e9c6b1b
Henry Bley <me@henrybley.com>
2025-06-08 14:08:24 +0200

    updates

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -9,11 +9,23 @@ in {
     programs.zsh = {
       enable = true;
 
-      oh-my-zsh = {
+      antidote = {
         enable = true;
-        theme = "xiong-chiamiov-plus";
-        plugins = [ "git" "zsh-users/zsh-autosuggestions" "zsh-users/zsh-syntax-highlighting" ];
+        package = pkgs.antidote;
+        plugins = [
+          "git"
+          "zsh-users/zsh-autosuggestions"
+          "zsh-users/zsh-syntax-highlighting"
+        ];
       };
+
+      shellInit = ''
+            # xiong-chiamiov-plus prompt
+            PROMPT=$'%{\e[0;34m%}%B┌─[%b%{\e[0m%}%{\e[1;32m%}%n%{\e[1;30m%}@%{\e[0m%}%{\e[0;36m%}%m%{\e[0;34m%}%B]%b%{\e[0m%} - %b%{\e[0;34m%}%B[%b%{\e[1;37m%}%~%{\e[0;34m%}%B]%b%{\e[0m%} - %{\e[0;34m%}%B[%b%{\e[0;33m%}'%D{"%a %b %d, %H:%M"}%b$'%{\e[0;34m%}%B]%b%{\e[0m%}
+        %{\e[0;34m%}%B└─%B[%{\e[1;35m%}$%{\e[0;34m%}%B] <$(git_prompt_info)>%{\e[0m%}%b '
+
+            PS2=$' \e[0;34m%}%B>%{\e[0m%}%b '
+      '';
     };
   };
 }
