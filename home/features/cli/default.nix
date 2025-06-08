9307c46734d33e6c95962adcf4c712dd046d5125
Henry Bley <me@henrybley.com>
2025-06-08 13:38:13 +0200

    updates

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -1,21 +1,22 @@
-{pkgs, ...}: {
-    programs.eza = {
-        enable = true;
-        extraOptions = ["-1" "--icons" "--git" "-a"];
-    };
+{ pkgs, ... }: {
+  imports = [ ./zsh.nix ];
+  programs.eza = {
+    enable = true;
+    extraOptions = [ "-1" "--icons" "--git" "-a" ];
+  };
 
-    programs.bat = {enable = true;};
+  programs.bat = { enable = true; };
 
-    home.packages = with pkgs; [
-        coreutils
-        fd
-        btop
-        httpie
-        jq
-        procs
-        ripgrep
-        tldr
-        zip
-    ];
+  home.packages = with pkgs; [
+    coreutils
+    fd
+    btop
+    httpie
+    jq
+    procs
+    ripgrep
+    tldr
+    zip
+  ];
 
 }
