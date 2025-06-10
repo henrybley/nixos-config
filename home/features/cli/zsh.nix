aa26ac7e033b1b10b31e981bc339c8540209096c
Henry Bley <me@henrybley.com>
2025-06-10 06:54:17 +0200

    updates

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -9,22 +9,33 @@ in {
     programs.zsh = {
       enable = true;
 
-      antidote = {
-        enable = true;
-        package = pkgs.antidote;
-        plugins = [
-          "git"
-          "zsh-users/zsh-autosuggestions"
-          "zsh-users/zsh-syntax-highlighting"
-        ];
+      # Install plugins with Nix
+      ohMyZsh = {
+        enable = false; # disable oh-my-zsh to avoid conflicts
       };
 
+      # Zsh plugins as packages
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.zsh-autosuggestions;
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.zsh-syntax-highlighting;
+        }
+      ];
+
       shellInit = ''
-            # xiong-chiamiov-plus prompt
-            PROMPT=$'%{\e[0;34m%}%B┌─[%b%{\e[0m%}%{\e[1;32m%}%n%{\e[1;30m%}@%{\e[0m%}%{\e[0;36m%}%m%{\e[0;34m%}%B]%b%{\e[0m%} - %b%{\e[0;34m%}%B[%b%{\e[1;37m%}%~%{\e[0;34m%}%B]%b%{\e[0m%} - %{\e[0;34m%}%B[%b%{\e[0;33m%}'%D{"%a %b %d, %H:%M"}%b$'%{\e[0;34m%}%B]%b%{\e[0m%}
+        # xiong-chiamiov-plus prompt
+        PROMPT=$'%{\e[0;34m%}%B┌─[%b%{\e[0m%}%{\e[1;32m%}%n%{\e[1;30m%}@%{\e[0m%}%{\e[0;36m%}%m%{\e[0;34m%}%B]%b%{\e[0m%} - %b%{\e[0;34m%}%B[%b%{\e[1;37m%}%~%{\e[0;34m%}%B]%b%{\e[0m%} - %{\e[0;34m%}%B[%b%{\e[0;33m%}'%D{"%a %b %d, %H:%M"}%b$'%{\e[0;34m%}%B]%b%{\e[0m%}
         %{\e[0;34m%}%B└─%B[%{\e[1;35m%}$%{\e[0;34m%}%B] <$(git_prompt_info)>%{\e[0m%}%b '
 
-            PS2=$' \e[0;34m%}%B>%{\e[0m%}%b '
+        PS2=$' \e[0;34m%}%B>%{\e[0m%}%b '
+
+        # Source plugins manually
+        source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+        source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
       '';
     };
   };
