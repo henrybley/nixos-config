f5cad07e64a1f687e7d6e094e570c24a8f0fe7cb
Henry Bley <me@henrybley.com>
2025-06-10 08:11:01 +0200

    updates

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -13,28 +13,38 @@ in {
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       initExtra = ''
-          setopt autocd # auto cd when only path is entered
-          setopt nomatch # throw an error on glob matching nothing
-          setopt nonomatch
-          setopt appendhistory
-          setopt hist_ignore_all_dups # remove older duplicate entries from history
-          setopt hist_reduce_blanks # remove superfluous blanks from history items
-          setopt inc_append_history # save history entries as soon as they are entered
-          setopt share_history # share history between different instances of the shell
-          setopt auto_cd # cd by typing directory name if it's not a command
-          unsetopt correct_all
-          setopt auto_list # automatically list choices on ambiguous completion
-          setopt auto_menu # automatically use menu completion
-          setopt always_to_end # move cursor to end if word had one match
-          setopt incappendhistory  #Immediately append to the history file, not just when a term is killed
-          zstyle ':completion:*' menu select # select completions with arrow keys
-          zstyle ':completion:*' group-name \'\' # group results by category
-          zstyle ':completion:::::' completer _expand _complete _ignored _approximate #enable approximate matches for completion
-          export PATH=$PATH:$HOME/.local/bin:/home/${username}/.cargo/bin:/home/${username}/.spicetify
-          export NIX_PAGER=cat
-          export PROMPT_EOL_MARK=" "
-          [ -f "$HOME/.config/zsh/colors_and_functions.zsh" ] && source $HOME/.config/zsh/colors_and_functions.zsh
+        setopt autocd # auto cd when only path is entered
+        setopt nomatch # throw an error on glob matching nothing
+        setopt nonomatch
+        setopt appendhistory
+        setopt hist_ignore_all_dups # remove older duplicate entries from history
+        setopt hist_reduce_blanks # remove superfluous blanks from history items
+        setopt inc_append_history # save history entries as soon as they are entered
+        setopt share_history # share history between different instances of the shell
+        setopt auto_cd # cd by typing directory name if it's not a command
+        unsetopt correct_all
+        setopt auto_list # automatically list choices on ambiguous completion
+        setopt auto_menu # automatically use menu completion
+        setopt always_to_end # move cursor to end if word had one match
+        setopt incappendhistory  #Immediately append to the history file, not just when a term is killed
+        zstyle ':completion:*' menu select # select completions with arrow keys
+        zstyle ':completion:*' group-name \'\' # group results by category
+        zstyle ':completion:::::' completer _expand _complete _ignored _approximate #enable approximate matches for completion
+        export PATH=$PATH:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.spicetify
+        export NIX_PAGER=cat
+        export PROMPT_EOL_MARK=" "
+        [ -f "$HOME/.config/zsh/colors_and_functions.zsh" ] && source $HOME/.config/zsh/colors_and_functions.zsh
       '';
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "sudo"
+          "history-substring-search"
+          "git"
+          "web-search"
+          "vi-mode"
+        ];
+      };
     };
   };
 }
