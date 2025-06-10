8cf79409c6aaf4b3d89f5fbf051279ccd38a0c32
Henry Bley <me@henrybley.com>
2025-06-10 06:57:14 +0200

    updates

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -26,7 +26,7 @@ in {
         }
       ];
 
-      shellInit = ''
+      interactiveShellInit = ''
         # xiong-chiamiov-plus prompt
         PROMPT=$'%{\e[0;34m%}%B┌─[%b%{\e[0m%}%{\e[1;32m%}%n%{\e[1;30m%}@%{\e[0m%}%{\e[0;36m%}%m%{\e[0;34m%}%B]%b%{\e[0m%} - %b%{\e[0;34m%}%B[%b%{\e[1;37m%}%~%{\e[0;34m%}%B]%b%{\e[0m%} - %{\e[0;34m%}%B[%b%{\e[0;33m%}'%D{"%a %b %d, %H:%M"}%b$'%{\e[0;34m%}%B]%b%{\e[0m%}
         %{\e[0;34m%}%B└─%B[%{\e[1;35m%}$%{\e[0;34m%}%B] <$(git_prompt_info)>%{\e[0m%}%b '
