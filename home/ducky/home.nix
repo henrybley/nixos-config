b62b7f816a9516ba682d0d32555a8e1fd0858fdd
Henry Bley <me@henrybley.com>
2025-06-14 20:39:54 +0200

    updates

--- home/ducky/home.nix
+++ home/ducky/home.nix
@@ -22,7 +22,8 @@
   stylix = {
     enable = true;
     autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme =
+            "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
     fonts = {
       sansSerif = {
         package = pkgs.nerd-fonts.jetbrains-mono;
