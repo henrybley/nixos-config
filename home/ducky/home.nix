2b4d5ca8f6352e0ea883abe6390d6a2bbceae198
Henry Bley <me@henrybley.com>
2025-06-14 20:03:03 +0200

    updates

--- home/ducky/home.nix
+++ home/ducky/home.nix
@@ -19,16 +19,7 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  stylix = {
-    enable = true;
-    base16Scheme = "gruvbox-dark-hard";
-    fonts = {
-      sansSerif = "JetBrains Mono";
-      monospace = "JetBrains Mono";
-    };
-
-    targets.kitty.enable = true;
-  };
+  
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
