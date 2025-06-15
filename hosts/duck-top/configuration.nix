0932879fb1ec83bc5ffd633af24a9e29c014946e
Henry Bley <me@henrybley.com>
2025-06-15 12:11:55 +0200

    updates

--- hosts/duck-top/configuration.nix
+++ hosts/duck-top/configuration.nix
@@ -41,7 +41,7 @@
     enable = true;
     autoEnable = true;
     base16Scheme =
-      "${pkgs.base16-schemes}/share/themes/monokai.yaml";
+      "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
     cursor = {
       package = pkgs.material-cursors;
       name = "material_cursors";
